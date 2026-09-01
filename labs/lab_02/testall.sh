84ba51571fb6675f973820f2ee3b27c44cabb382
JimmyChin946 <Judes946@gmail.com>
2026-09-01 10:12:48 -0700

    testing

---
+++ labs/lab_02/testall.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+# Regular large triangle
+exec ./build/Lab02 ./build/regular.png 100 100 1 1 5 80 90 90
+
+# Collapses into a line
+exec ./build/Lab02 ./build/line.png 100 100 1 1 50 50 90 90
+
+# Collapses into a point
+exec ./build/Lab02 ./build/point.png 100 100 1 1 1 1 1 1
+
