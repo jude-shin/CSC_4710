71fcdba0535872d899ff726766d1924d6bd91056
JimmyChin946 <Judes946@gmail.com>
2026-09-07 11:48:39 -0700

    point and traingle code from lab02

---
+++ programs/prog_1/src/Point.cpp
@@ -0,0 +1,46 @@
+#include <cassert>
+#include <stdexcept>
+#include "Point.h"
+#include "Image.h"
+
+using namespace std;
+
+Point::Point(int x, int y, int r, int g, int b) 
+	: x(x), y(y), r(r), g(g), b(b) {
+	// Ensure the point is within a valid 3D workspace boundary
+	if (r < 0 || r > 255) {
+		throw std::invalid_argument("r value not within [0, 255]");
+	}
+
+	if (g < 0 || g > 255) {
+		throw std::invalid_argument("g value not within [0, 255]");
+	}
+
+	if (b < 0 || b > 255) {
+		throw std::invalid_argument("b value not within [0, 255]");
+	}
+}
+
+Point::Point(const Point& other) {
+	x = other.x;
+	y = other.y;
+	r = other.r;
+	g = other.g;
+	b = other.b;
+}
+
+Point::Point()
+	: x(0), y(0), r(0), g(0), b(255) {
+}
+
+
+int Point::get_x() const { return x; }
+int Point::get_y() const { return y; }
+int Point::get_r() const { return r; }
+int Point::get_g() const { return g; }
+int Point::get_b() const { return b; }
+
+
+void Point::draw_points(Image* image) {
+	image->setPixel(x, y, r, g, b);
+}
---
+++ programs/prog_1/src/Triangle.h
@@ -0,0 +1,23 @@
+#pragma once
+#ifndef _TRIANGLE_H_
+#define _TRIANGLE_H_
+
+#include <string>
+#include <vector>
+#include "Image.h" 
+#include "Point.h" 
+
+class Triangle {
+public:
+  Triangle(Point* a, Point* b, Point* c);
+  void draw_bounding_box(Image* image);
+  void draw_points(Image* image);
+  void draw_triangle(Image* image);
+
+private:
+  Point a;
+  Point b;
+  Point c;
+};
+
+#endif
